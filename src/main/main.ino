9fe483dd951b1e3610255fbad8975a4952d6d92d
Ignatij3 <ignatijs.krasovskis@gmail.com>
2023-09-22 14:20:14 +0200

    Almost finished serial, finished testing code

--- src/main/main.ino
+++ src/main/main.ino
@@ -2,10 +2,7 @@ extern "C" {
 #include "src/central_controller/central_controller.h"
 }
 
-void setup() {
-    Serial.begin(9600);
-    initializeVehicleControls(10);
-}
+void setup() { initializeModules(10); }
 
 void loop() {
     setMode(AUTOMATIC);
