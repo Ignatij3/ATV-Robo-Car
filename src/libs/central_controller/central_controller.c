eccdbb6639c913fd7a7f12d6e90cd19d5ae76024
Ignatij3 <ignatijs.krasovskis@gmail.com>
2023-10-20 18:41:12 +0200

    Finished merging

--- src/libs/central_controller/central_controller.c
+++ src/libs/central_controller/central_controller.c
@@ -5,13 +5,10 @@
 #include "../oled/ssd1306.h"
 #include "../serial_communication/serial_communication.h"
 #include "central_controller.h"
-=======
-#include "distance_sensor/HCSR04.h"
-    >>>>>>> e454b2a7b4f3094f0744b9f54dbad4707cdabaf4
 
 #define MYUBRR(baud) CPU_CLOCK / 16 / baud - 1
 
-    static uint8_t minDist;
+static uint8_t minDist;
 static drivingMode mode;
 
 // initializeModules performs initialization of structures for the vehicle to operate.
