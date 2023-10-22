e810e4214df5a0c677ce913454242d15e3a8663c
Ignatij3 <ignatijs.krasovskis@gmail.com>
2023-10-22 02:33:22 +0200

    Changed write to serial

--- src/libs/joystick/joystick.c
+++ src/libs/joystick/joystick.c
@@ -1,7 +1,8 @@
-#include "../oled/ssd1306.h"
 #include "../ino_libs/ino_libs.h"
-#include <util/delay.h>
+#include "../oled/ssd1306.h"
+#include "../serial_communication/serial_communication.h"
 #include <stdio.h>
+#include <util/delay.h>
 
 #define VRX PINC1
 #define VRY PINC0
@@ -13,13 +14,12 @@ void registerJoystick(void) {
     pinMode(&PORTC, VRY, INPUT);
 }
 
-void lol(){
-    char str[20]; 
-    while(1){
+void lol() {
+    char str[20];
+    while (1) {
         int xValue = analogRead(&PORTC, VRX);
         int yValue = analogRead(&PORTC, VRY);
-        sprintf(str, "%d", xValue);
-        setSpeed_OLED(str);
-        delay_ms(1000);
+        writeStringF("x: %d | y: %d\n\r", xValue, yValue);
+        _delay_ms(1000);
     }
- }
+}
