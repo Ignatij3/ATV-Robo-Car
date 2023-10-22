6e43c608dcd4d324bc9c5c218d6486bdc4cf2761
Ignatij3 <ignatijs.krasovskis@gmail.com>
2023-10-22 21:42:16 +0200

    Now both sides' speeds are tracked to ensure correct steering while moving is possible

--- src/libs/engine_controller/engine_controller.h
+++ src/libs/engine_controller/engine_controller.h
@@ -8,10 +8,14 @@ void setEnginesDirection(bool reverse);
 void turnOffEngines(void);
 void turnLeft(bool (*cancelFunc)(void));
 void turnRight(bool (*cancelFunc)(void));
+void setLeftSpeed(uint8_t speed, bool reverse);
+void setRightSpeed(uint8_t speed, bool reverse);
 void setSpeed(uint8_t speed, bool reverse);
 void increaseSpeed(uint8_t speed);
 void decreaseSpeed(uint8_t speed);
 uint8_t getSpeed(void);
+uint8_t getLeftSpeed(void);
+uint8_t getRightSpeed(void);
 bool isReverse(void);
 
 #endif // ATV_ENGINE_CONTROLLER_ENGINE_CONTROLLER_H
