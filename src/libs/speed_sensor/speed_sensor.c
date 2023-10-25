2c0d1b5cb0c30b9851ef6142238b69ff18944d8a
Vladimir <vurcnok@gmail.com>
2023-10-25 13:45:18 +0200

    millis and macros

--- src/libs/speed_sensor/speed_sensor.c
+++ src/libs/speed_sensor/speed_sensor.c
@@ -9,45 +9,80 @@ void registerSpeedSensor(void){
     pinMode(&PORTD, SPEED_SENSOR, INPUT_PULLUP);
 }
 
-volatile uint32_t milliseconds = 0;
+// volatile uint32_t milliseconds = 0;
 
-// Function to initialize the timer and enable interrupts
-void initTimer() {
-    // Set up timer 0
-    TCCR0B = (1 << CS01);
+// // Function to initialize the timer and enable interrupts
+// void initTimer() {
+//     // Set up timer 0
+//     TCCR0B = (1 << CS01);
 
-    // Enable overflow interrupt
-    TIMSK0 |= (1 << TOIE0);
+//     // Enable overflow interrupt
+//     TIMSK0 |= (1 << TOIE0);
 
-    // Activate interrupts
-    sei();
-}
+//     // Activate interrupts
+//     sei();
+// }
 
-// Function to get the current time in milliseconds
-uint32_t millis() {
-    uint32_t ms;
-    cli();
-    ms = milliseconds;
-    sei();
-    return ms;
-}
+// // Function to get the current time in milliseconds
+// uint32_t millis() {
+//     uint32_t ms;
+//     cli();
+//     ms = milliseconds;
+//     sei();
+//     return ms;
+// }
 
-ISR(TIMER0_OVF_vect) {
-    // This gets called every time there is an overflow in the timer register
-    milliseconds += 1; // Increase milliseconds by 1
-}
+// ISR(TIMER0_OVF_vect) {
+//     // This gets called every time there is an overflow in the timer register
+//     milliseconds += 1; // Increase milliseconds by 1
+// }
 
-void updateSpeed(void){
+// void updateSpeed(void){
     
-    while (!digitalRead(&PORTD, SPEED_SENSOR)){
-    }
-    initTimer();
-    uint32_t currentTime = millis();
-    while (digitalRead(&PORTD, SPEED_SENSOR)){
+//     while (!digitalRead(&PORTD, SPEED_SENSOR)){
+//     }
+//     initTimer();
+//     uint32_t currentTime = millis();
+//     while (digitalRead(&PORTD, SPEED_SENSOR)){
         
-    }
-    uint32_t elapseTime = millis()-currentTime;
-    //formula 
-    //setSpeed_OLED(formula);
-    return;
-}
+//     }
+//     uint32_t elapseTime = millis()-currentTime;
+//     //formula 
+//     //setSpeed_OLED(formula);
+//     return;
+// }
+
+// #define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
+
+// volatile unsigned long timer0_overflow_count = 0;
+// volatile unsigned long timer0_millis = 0;
+
+// unsigned long millis()
+// {
+// 	unsigned long m;
+// 	uint8_t oldSREG = SREG;
+
+// 	// disable interrupts while we read timer0_millis or we might get an
+// 	// inconsistent value (e.g. in the middle of a write to timer0_millis)
+// 	cli();
+// 	m = timer0_millis;
+// 	SREG = oldSREG;
+
+// 	return m;
+// }
+
+// unsigned long micros() {
+// 	unsigned long m;
+// 	uint8_t oldSREG = SREG, t;
+	
+// 	cli();
+// 	m = timer0_overflow_count;
+// 	#error TIMER 0 not defined
+
+// 	if ((TIFR & _BV(TOV0)) && (t < 255))
+// 		m++;
+
+// 	SREG = oldSREG;
+	
+// 	return ((m << 8) + t) * (64 / clockCyclesPerMicrosecond());
+// }
--- src/main.c
+++ src/main.c
@@ -32,66 +32,114 @@ void setUpInterrupts(void) {
     sei();                 // enable interrupts
 }
 
-int main(void) {
-    initializeModules(10);
-    setUpInterrupts();
-    setMode(readNewMode());
-    enableCar();
-    time_t timeNow = time(NULL), previousTime = 0;  
+#define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
 
-    while (1) {
-        timeNow = time(NULL); //get new time
-        //every second the timer and speed information are updated
-        if (timeNow - previousTime >= INTERVAL){ 
-            previousTime = timeNow;
-            updateCarSpeed();
-            updateTimer();
-        }
+volatile unsigned long timer0_overflow_count = 0;
+volatile unsigned long timer0_millis = 0;
 
-        // halt while car is turned off
-        if (!poweredOn) {
-            disableCar();
-            while (!poweredOn) {
-            }
-            enableCar();
-        }
+unsigned long millis()
+{
+	unsigned long m;
+	uint8_t oldSREG = SREG;
 
-        //change driving mode when joystick is pressed
-        if (joystickPressed()){
-            setMode(readNewMode());
-        }
+	// disable interrupts while we read timer0_millis or we might get an
+	// inconsistent value (e.g. in the middle of a write to timer0_millis)
+	cli();
+	m = timer0_millis;
+	SREG = oldSREG;
 
-        switch (getMode()) {
-        // in automatic mode, car drives forward until colliding
-        // afterwards, it turns around and continues forward
-        case AUTOMATIC:
-            accelerate(1);
-            // if (isCollisionSoon()) {
-            //     evadeCollision();
-            // }
-            break;
-
-        // in controlled mode, car receives and executes commands from DualShock PS4 controller
-        case CONTROLLED:
-            /* code */
-            break;
-
-        // in slave mode, car follows black line. If there is predecessor on a line, the car tailgates it
-        case SLAVE:
-            updateLinePosition();
-            adjustEnginesSpeed(30);
-            break;
-
-        // if NONE mode is chosen, the car must halt
-        case NONE:
-            goto exit;
-        }
-        // delay not to change state too rapidly
-        _delay_ms(10);
-    }
-
-exit:
-    disableCar();
-    writeString("\n\rerror occured, exiting");
-    return 1;
+	return m;
 }
+
+unsigned long micros() {
+	unsigned long m;
+	uint8_t oldSREG = SREG, t;
+	
+	cli();
+    t = TCNT0;
+	m = timer0_overflow_count;
+	if ((TIFR0 & _BV(TOV0)) && (t < 255))
+		m++;
+
+	SREG = oldSREG;
+	
+	return ((m << 8) + t) * (64 / clockCyclesPerMicrosecond());
+}
+
+int main(void) {
+    uint16_t time1 = millis();
+    uint16_t time2 = micros();
+    _delay_ms(1023);
+    uint16_t time3 = millis();
+    uint16_t time4 = micros();
+    writeUint(time1);
+    writeUint(time2);
+    writeUint(time3);
+    writeUint(time4);
+}
+
+
+
+
+//     initializeModules(10);
+//     setUpInterrupts();
+//     setMode(readNewMode());
+//     enableCar();
+//     time_t timeNow = time(NULL), previousTime = 0;  
+
+//     while (1) {
+//         timeNow = time(NULL); //get new time
+//         //every second the timer and speed information are updated
+//         if (timeNow - previousTime >= INTERVAL){ 
+//             previousTime = timeNow;
+//             updateCarSpeed();
+//             updateTimer();
+//         }
+
+//         // halt while car is turned off
+//         if (!poweredOn) {
+//             disableCar();
+//             while (!poweredOn) {
+//             }
+//             enableCar();
+//         }
+
+//         //change driving mode when joystick is pressed
+//         if (joystickPressed()){
+//             setMode(readNewMode());
+//         }
+
+//         switch (getMode()) {
+//         // in automatic mode, car drives forward until colliding
+//         // afterwards, it turns around and continues forward
+//         case AUTOMATIC:
+//             accelerate(1);
+//             // if (isCollisionSoon()) {
+//             //     evadeCollision();
+//             // }
+//             break;
+
+//         // in controlled mode, car receives and executes commands from DualShock PS4 controller
+//         case CONTROLLED:
+//             /* code */
+//             break;
+
+//         // in slave mode, car follows black line. If there is predecessor on a line, the car tailgates it
+//         case SLAVE:
+//             updateLinePosition();
+//             adjustEnginesSpeed(30);
+//             break;
+
+//         // if NONE mode is chosen, the car must halt
+//         case NONE:
+//             goto exit;
+//         }
+//         // delay not to change state too rapidly
+//         _delay_ms(10);
+//     }
+
+// exit:
+//     disableCar();
+//     writeString("\n\rerror occured, exiting");
+//     return 1;
+// }
