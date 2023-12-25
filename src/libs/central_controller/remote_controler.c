186c92dfbaa1f71aff340200b33dea819fc0fa90
Ignatij3 <ignatijs.krasovskis@gmail.com>
2023-12-25 23:16:12 +0100

    Removed conflict marker

--- src/libs/central_controller/remote_controler.c
+++ src/libs/central_controller/remote_controler.c
@@ -2,11 +2,6 @@
 #include "../serial_communication/serial_communication.h"
 #include "central_controller.h"
 
-<<<<<<< HEAD
-#define MYUBRR(baud) CPU_CLOCK / 16 / baud - 1
-
-=======
->>>>>>> origin/main
 // _controllerInitSerial will initialize UART communication.
 void _controllerInitSerial(void) {
     serialInit(DEFAULT_SERIAL_BAUD);
