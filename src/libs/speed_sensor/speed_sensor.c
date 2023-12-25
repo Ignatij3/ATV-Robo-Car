ea463ec09f6f14e43bddb164e5036141cec66e34
Ignatij3 <ignatijs.krasovskis@gmail.com>
2023-12-25 22:17:38 +0100

    Distance sensor fixes

--- src/libs/speed_sensor/speed_sensor.c
+++ src/libs/speed_sensor/speed_sensor.c
@@ -1,5 +1,11 @@
 #include "speed_sensor.h"
+#include "../engine_controller/engine_controller.h"
+
+#define x (double)(getSpeed() - 120)
+#define a 0.0001
+#define b 0.02
+#define c 1.0
 
 uint8_t getSensorSpeedReading(void) {
-    return 0; // insert formula here
+    return (uint8_t)(a * x * x + b * x + c);
 }
