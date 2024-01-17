15487f7273a79777e8a2ad2538d913515db27e5d
Ignatij3 <ignatijs.krasovskis@gmail.com>
2024-01-17 13:36:32 +0100

    Removed bloat from remote controller

--- src/libs/remote_control/remote_control.c
+++ src/libs/remote_control/remote_control.c
@@ -15,38 +15,7 @@ static volatile moveVector vec;
 
 // initModule will initialize variables to start listening for commands.
 void initModule(void) {
-    writeString("AT");
-    _delay_ms(COMMAND_COOLDOWN_MS);
-    writeString("AT+NAMEcar_controller");
-    _delay_ms(COMMAND_COOLDOWN_MS);
-    writeString("AT+PIN7800");
-    _delay_ms(COMMAND_COOLDOWN_MS);
     vec.direction = '\0';
-}
-
-void _OLD_initModule(void) {
-    char *s = malloc(sizeof(char) * 10);
-
-    writeString("AT");
-    _delay_ms(COMMAND_COOLDOWN_MS);
-    while (readCount() < 2)
-        ;
-    readNBytes(s, 2);
-
-    writeString("AT+NAMEcar_controller");
-    _delay_ms(COMMAND_COOLDOWN_MS);
-    while (readCount() < 6)
-        ;
-    readNBytes(s, 9);
-
-    writeString("AT+PIN7400");
-    _delay_ms(COMMAND_COOLDOWN_MS);
-    while (readCount() < 5)
-        ;
-    readNBytes(s, 8);
-
-    vec.direction = ' ';
-    free(s);
 }
 
 // readMovementCommand will read byte from serial indicating what direction is to drive.
