3a1741f2fa5574c905d9a1230b4fb47d964c552e
Ignatij3 <ignatijs.krasovskis@gmail.com>
2024-01-19 11:36:33 +0100

    fixed incorrect distance readings

--- src/libs/distance_sensor/HCSR04.c
+++ src/libs/distance_sensor/HCSR04.c
@@ -32,8 +32,10 @@ uint8_t measureDistanceCm(void) {
     // 18586.0035551 = 2.5 * MAX_DISTANCE / 0.0343000042 (rounded up to nearest tenth thousand)
     uint16_t distanceCm = (uint16_t)((pulseIn(&PORTB, ECHO_PIN, HIGH, 20000, sendPulse) * 1715) / 100000);
 
-    if (distanceCm == MIN_DISTANCE || distanceCm > MAX_DISTANCE) {
+    if (distanceCm <= MIN_DISTANCE) {
         return MIN_DISTANCE;
+    } else if (distanceCm >= MAX_DISTANCE) {
+        return MAX_DISTANCE;
     }
 
     return (uint8_t)(distanceCm);
