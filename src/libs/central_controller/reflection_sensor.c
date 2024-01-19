8a27febd29e7b3b7e052b8ab5dd8be8eff3cac0b
Ignatij3 <ignatijs.krasovskis@gmail.com>
2024-01-19 16:55:41 +0100

    Added more agility to the car

--- src/libs/central_controller/reflection_sensor.c
+++ src/libs/central_controller/reflection_sensor.c
@@ -6,7 +6,7 @@
 int8_t deviation;
 
 #define MAX_LINE_SPEED MAX_SPEED - 55
-#define MIN_LINE_SPEED MIN_SPEED + 100
+#define MIN_LINE_SPEED MIN_SPEED
 
 // _controllerInitIRSensor initializes IR reflectivity sensor.
 void _controllerInitIRSensor(void) {
